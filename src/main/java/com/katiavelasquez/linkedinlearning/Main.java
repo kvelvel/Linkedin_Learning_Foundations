70ed42be40e016f871e67c5c16585f378250a184
Katia Velasquez <katia.velasquez@gmail.com>
2025-07-09 14:11:55 -0400

    feat(new branch): Se crea rama nueva para ejercicio de tablero de bingo

--- src/main/java/com/katiavelasquez/linkedinlearning/Main.java
+++ src/main/java/com/katiavelasquez/linkedinlearning/Main.java
@@ -1,27 +1,28 @@
 package com.katiavelasquez.linkedinlearning;
 
-import com.katiavelasquez.linkedinlearning.basics.boletoLoteria.BoletoLoteria;
-import com.katiavelasquez.linkedinlearning.basics.boletoLoteria.BoletoLoteriaLista;
+import com.katiavelasquez.linkedinlearning.basics.boletoLoteria.*;
+import com.katiavelasquez.linkedinlearning.basics.boletoLoteriaClass.BoletoLoteriaClass;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
+
 public class Main
 {
     public static void main( String[] args )
     {
         BoletoLoteria boleto = new BoletoLoteria();
         int[] boletoGenerado = boleto.generarBoleto();
+        BoletoLoteriaClass boletoClase = new BoletoLoteriaClass();
 
         BoletoLoteriaLista boletoLista = new BoletoLoteriaLista();
         List<Integer> boletoListaGenerado = boletoLista.generarBoleto();
+        boletoClase.generarBoleto();
 
         System.out.println("Su boleto generado por array es el siguiente: " + Arrays.toString(boletoGenerado));
         System.out.println("Su boleto generado por Lista es el siguiente: " + boletoListaGenerado);
+        System.out.println("Su boleto generado como clase es el siguiente: " + boletoClase.obtenerBoleto());
+        System.out.println("Su numero de reintegro es el siguiente: " + boletoClase.obtenerNumeroReintegro());
     }
 }
